041307b7767570f9f422cf53c3f13cde20c7e6ac
Bektosh Madaminov <madaminovsbektosh@gmail.com>
2022-09-07 11:18:00 +0500

    added journalpb

--- scripts/generate-protos.sh
+++ scripts/generate-protos.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 protoc --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-    teacherpb/teacher.proto studentpb/student.proto schedulepb/schedule.proto
+    teacherpb/teacher.proto studentpb/student.proto \
+    schedulepb/schedule.proto journalpb/journal.proto
